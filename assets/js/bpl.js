bc199077978a4bc545a7352f73a5d01e04576869
Asko Nõmm <asko@digitalpress.co>
2017-01-11 05:49:11 +0200

    bump

---
+++ assets/js/bpl.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+	// like a post
+	$('.bpl-like-post').on('click', function(event) {
+
+		event.preventDefault();
+
+		// todo, ajax call
+
+		return false;
+
+	});
+
+});
